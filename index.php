0eecc061708ae3be79d3cfb8658c70b40f9cf75a
tsai <lovez81427@gmail.com>
2023-10-02 10:45:03 +0800

    php基礎-判斷if.elso

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$age='a=50;';
+$name='b=10;';
+
+echo $age;
+echo $name;
+
+$tmp=$age;
+$age=$name;
+$name=$tmp;
+
+// echo $age;
+// echo $name;
+?> 
+</body>
+</html>
