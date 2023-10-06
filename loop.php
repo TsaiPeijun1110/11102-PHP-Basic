f6ae040e17bc1b45a24418db53a97cdc20d6b27d
tsai <lovez81427@gmail.com>
2023-10-06 11:10:03 +0800

    迴圈數列練習

--- loop.php
+++ loop.php
@@ -5,14 +5,60 @@
 <?php
 $sum=0;
 
-for($i=1;$i<=10000;$i=$i++){
+for($i=1;$i<=1000;$i=$i+1)
+    for($i=1;$i<=10;$i=$i+1){
     // $sum=$sum+$i;
     echo '當$i='.$i."時";
     echo '$sum+$i的結果是';
-    echo $sum ."+".$i."=";
-    echo $sum+$i;
-    echo '<br>';
-    $sum += $i;
+    echo $sum ."+".$i;
+    }
+    
+echo "1加到1000為" .$sum;
+echo "1加到10為" .$sum;
+
+?>
+
+<h1>迴圈練習</h1>
+<3>使用for迴圈來產生以下的數列</3>
+<ul>
+    <li>1,3,5,7,9</li>
+    <li>10,20,30,40,50,60……n</li>
+    <li>3,5,7,11,13,17……97</li>
+</ul>
+
+<?php
+$n=30;
+
+
+for($i=1;$i<=$n;$i=$i+2){
+    echo $i;
+    echo ",";
+}
+echo "<hr>";
+for($i=1;$i<=$n;$i++){
+    echo $i*10;
+    echo ",";
+}
+echo "<hr>";
+$a=98;
+
+$flag=true;
+For($i=2;$i<($a/2);$i++){
+    echo "$a 除以 $i 餘數". ($a % $i);
+    if(($a % $i) == 0){
+        $flag=false;
+        echo"<br>";
+        break;       
+    }
+     echo "<br>";   
 }
 
-echo "1加到10000為" .$sum;
+if($flag==true){
+    echo $a ."是質數";
+}else{
+    echo $a ."不是質數";
+}
+
+
+
+?>
