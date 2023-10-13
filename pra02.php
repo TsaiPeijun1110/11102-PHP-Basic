7a0e1ed8b0f24947310651ef1c02d7120dd26316
tsai <lovez81427@gmail.com>
2023-10-13 11:10:40 +0800

    九九乘法表-留左下

--- pra02.php
+++ pra02.php
@@ -1,7 +1,7 @@
 <style>
     table{
         border-collapse: collapse;
-        border: 1px solid black;
+        border: 1px solid palevioletred;
     }
     td{
         border: 1px solid thistle;
@@ -61,6 +61,45 @@ for($j=0;$j<10;$j++){
     echo "</tr>";
 }
 echo "</table>";
+?>
+
+
+<h3>99乘法表-留左下區</h3>
+<?php
+echo "<table>";
+echo "<tr>";
+for($j=0;$j<10;$j++){
+    if($j==0){
+        echo "<tr style='background:#ccc'>";
+    }else{
+    echo "<tr>";
+}
+    for($i=0;$i<10;$i++){
+        if($i==0){
+            echo "<td style='background:#ccc'>";
+        }else{
+        echo "<td>";
+        }
+        if($i==0 && $j==0){
+            echo "";
+        }else if($j==0){
+            echo $i;
+        }else if($i==0){
+            echo $j;
+        }else if($j<$i){ 
+        echo "";
+        }else{
+        echo $j*$i;
+        }
+        echo "</td>";      
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
+
+
+
 
 
 
