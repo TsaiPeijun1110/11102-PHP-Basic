fd1e1be88326050d7e9442ecb4878069b765ddb7
tsai <lovez81427@gmail.com>
2023-10-13 14:14:09 +0800

    倒正三角形

--- pra04.php
+++ pra04.php
@@ -45,6 +45,19 @@ for($i=0;$i<5;$i++){
     echo "<br>";
 }
 echo "<hr>";
-    
+?>
+
+<h2>倒正三角形</h2>
+<?php
+for($i=4;$i>=0;$i--){
+    for($j=0;$j<(4-$i);$j++){
+        echo "&nbsp;";
+    }
+    for($k=0;$k<($i*2+1);$k++){
+        echo "*";
+    }
+    echo "<br>";
+}
+echo "<hr>";
 
 ?>
