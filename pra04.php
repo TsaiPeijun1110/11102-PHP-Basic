d81022952b2e5cb6c18f296f55dfda317751fc37
tsai <lovez81427@gmail.com>
2023-10-13 15:25:58 +0800

    矩形

--- pra04.php
+++ pra04.php
@@ -83,3 +83,24 @@ for($i=0;$i<9;$i++){
 echo "<hr>";
 
 ?>
+
+<h2>矩形</h2>
+<?php
+
+for($i=0;$i<7;$i++){
+
+    for($j=0;$j<7;$j++){
+        if($i==0 || $i==6){
+            echo "*";
+        }else if($j==0 || $j==6){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }
+        
+    }
+
+    echo "<br>";
+}
+echo "<hr>";
+?>
