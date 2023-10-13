fa6e6258793b41a2d5e29661bad3ad69004e7240
tsai <lovez81427@gmail.com>
2023-10-13 15:54:53 +0800

    內含對角線的矩形+css

--- pra04.php
+++ pra04.php
@@ -1,7 +1,7 @@
 
 <style>
 h2{
-    color: thistle;
+    color: palevioletred;
 }
 </style>
 
@@ -131,5 +131,26 @@ for($i=0;$i<7;$i++){
 }
 echo "<hr>";
 
+
+?>
+<h2>內含對角線的矩形+css</h2>
+<?php
+for($i=0;$i<7;$i++){
+
+    for($j=0;$j<7;$j++){
+        if($i==0 || $i==6){
+            echo "*";
+        }else if($j==0 || $j==6){
+            echo "*";
+        }else if($j==$i || $i+$j==6){
+            echo "<span style='color:palevioletred'>*</span>";
+        }else{
+            echo "&nbsp;";
+        }
+        
+    }
+    echo "<br>";
+}
+echo "<hr>";
 
 ?>
