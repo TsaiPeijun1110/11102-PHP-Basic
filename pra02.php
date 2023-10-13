a61da904576c4f69536e9e1847dbb7e4ff7b2783
tsai <lovez81427@gmail.com>
2023-10-13 10:59:51 +0800

    九九乘法表-變化型

--- pra02.php
+++ pra02.php
@@ -30,7 +30,37 @@ for($j=1;$j<=9;$j++){
 echo "</table>";
 
 ?>
+<h3>99乘法表-有標頭</h3>
+<?php
+echo "<table>";
+echo "<tr>";
+for($j=0;$j<10;$j++){
+    if($j==0){
+        echo "<tr style='background:#ccc'>";
+    }else{
+    echo "<tr>";
+}
+    for($i=0;$i<10;$i++){
+        if($i==0){
+            echo "<td style='background:#ccc'>";
+        }else{
+        echo "<td>";
+        }
+        if($i==0 && $j==0){
+            echo "";
+        }else if($j==0){
+            echo $i;
+        }else if($i==0){
+            echo $j;
+        }else{
+        echo $j*$i;
+        }
 
+        echo "</td>";      
+    }
+    echo "</tr>";
+}
+echo "</table>";
 
 
 
