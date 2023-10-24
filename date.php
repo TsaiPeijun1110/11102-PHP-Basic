bfb5f017bc71caefdd52db395cfd8a12e78b6d2b
tsai <lovez81427@gmail.com>
2023-10-24 10:45:16 +0800

    利用date()函式的格式化參數，完成以下的日期格式呈現

--- date.php
+++ date.php
@@ -32,5 +32,31 @@ if($diff>$today){
     $days=($diff-$today)/(60*60*24);
 }
 echo "距離下一次的生日:".date("Y-m-d",$diff)."還有". floor($days)."天";
- 
+?>
 
+<h2>利用date()函式的格式化參數，完成以下的日期格式呈現</h2>
+<ul>
+    <li>2021/10/05</li>
+    <li>10月5日 Tuesday</li>
+    <li>2021-10-5 12:9:5</li>
+    <li>2021-10-5 12:09:05</li>
+    <li>今天是西元2021年10月5日 上班日(或假日)</li>
+</ul>
+<?php
+echo $date="2021/10/05";
+echo "<br>";
+echo date("m-d l");
+echo "<br>";
+echo date("Y-m-d H:i:s");
+echo "<br>";
+echo date("Y-n-j G:i:s");
+echo "<br>";
+
+echo date("今天是西元Y年m月d日");
+if(date("N")<=5){
+    echo "上班日";
+}else{
+    echo "假日";
+}
+?>
+<h2></h2>~
